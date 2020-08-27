81bc26a8b293497bef462b2e6c5d688cf113d4f9
cckn <pds@enitt.co.kr>
2020-08-27 19:51:01 +0900

    add convert/float

---
+++ convert/float.go
@@ -0,0 +1,20 @@
+package convert
+
+import (
+	"encoding/binary"
+	"math"
+)
+
+// Float32FromBytes
+func Float32FromBytes(bytes []byte) float32 {
+	bits := binary.BigEndian.Uint32(bytes)
+	float := math.Float32frombits(bits)
+	return float
+}
+
+func Float32ToBytes(float float32) []byte {
+	bits := math.Float32bits(float)
+	bytes := make([]byte, 8)
+	binary.BigEndian.PutUint32(bytes, bits)
+	return bytes
+}
--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module "github.com/cckn/go_utils`"
+
+go 1.14
