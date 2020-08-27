931c0f9a49982ceb7dba5048fe0684e8ed543301
cckn <pds@enitt.co.kr>
2020-08-27 20:49:12 +0900

    add float.go, string.go

--- convert/float.go
+++ convert/float.go
@@ -5,16 +5,32 @@ import (
 	"math"
 )
 
-// Float32FromBytes
-func Float32FromBytes(bytes []byte) float32 {
+// BytesToFloat32
+func BytesToFloat32(bytes []byte) float32 {
 	bits := binary.BigEndian.Uint32(bytes)
 	float := math.Float32frombits(bits)
 	return float
 }
 
+// BytesToFloat64
+func BytesToFloat64(bytes []byte) float64 {
+	bits := binary.BigEndian.Uint64(bytes)
+	float := math.Float64frombits(bits)
+	return float
+}
+
+// Float32ToBytes
 func Float32ToBytes(float float32) []byte {
 	bits := math.Float32bits(float)
-	bytes := make([]byte, 8)
+	bytes := make([]byte, 4)
 	binary.BigEndian.PutUint32(bytes, bits)
+	return bytes
+}
+
+// Float64ToBytes
+func Float64ToBytes(float float64) []byte {
+	bits := math.Float64bits(float)
+	bytes := make([]byte, 8)
+	binary.BigEndian.PutUint64(bytes, bits)
 	return bytes
 }
---
+++ convert/string.go
@@ -0,0 +1,32 @@
+package convert
+
+import (
+	"encoding/hex"
+	"log"
+	"strings"
+)
+
+func BytesToHexStrings(b []byte) []string {
+	var result []string
+	s := hex.EncodeToString(b)
+
+	for i := 0; i < len(s); i = i + 2 {
+		result = append(result, s[i:i+2])
+	}
+
+	return result
+}
+
+func StringsToBytes(s []string) []byte {
+	var result []byte
+
+	str := strings.Join(s, "")
+
+	result, err := hex.DecodeString(str)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return result
+}
